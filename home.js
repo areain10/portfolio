b2b2f730da7fedf1edaecd3664e951162a2b4335
areain10 <107684386+areain10@users.noreply.github.com>
2022-12-29 14:36:32 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,109 @@
+let intro = document.querySelector('.intro');
+let name = document.querySelector('.name-header');
+let nameSpan = document.querySelectorAll('.name');
+
+window.addEventListener('DOMContentLoaded',()=>{
+        setTimeout(()=>{
+                nameSpan.forEach((span,idx)=>{
+                        setTimeout(()=>{
+                                span.classList.add('active');
+                                setTimeout(()=>{
+                                },300)
+                        },(idx+1)*400)
+                })
+        });
+
+        setTimeout(()=>{
+                nameSpan.forEach((span,idx)=>{
+                        setTimeout(()=>{
+                                setTimeout(()=>{
+
+                                        span.classList.remove('active');
+                                        span.classList.add('fade');
+                                },300)
+                        },(idx+1)*400)
+                })
+        },2000);
+
+        setTimeout(()=>{
+                intro.style.top= '-100vh';
+        },4000);
+});
+
+document.querySelector("#abouticon").addEventListener("click",()=>{
+	elmnt = document.querySelector("#about")
+	if(elmnt.style.display === "none"){
+		elmnt.style.display="flex";
+	}else{
+		elmnt.style.display="none";
+	};
+});
+document.querySelector("#porticon").addEventListener("click",()=>{
+	elmnt = document.querySelector("#port")
+	if(elmnt.style.display === "none"){
+		elmnt.style.display="flex";
+	}else{
+		elmnt.style.display="none";
+	};
+});
+document.querySelector("#serviceicon").addEventListener("click",()=>{
+	elmnt = document.querySelector("#service")
+	if(elmnt.style.display === "none"){
+		elmnt.style.display="flex";
+	}else{
+		elmnt.style.display="none";
+	};
+});
+document.querySelector("#contacticon").addEventListener("click",()=>{
+	elmnt = document.querySelector("#contact")
+	if(elmnt.style.display === "none"){
+		elmnt.style.display="flex";
+	}else{
+		elmnt.style.display="none";
+	};
+});
+
+dragElement(document.querySelector("#about"));
+dragElement(document.querySelector("#port"));
+dragElement(document.querySelector("#sevice"));
+dragElement(document.querySelector("contact"));
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
